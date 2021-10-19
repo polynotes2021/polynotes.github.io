1eadc707703192f1e05921daef3c9244ae576b9f
Poly Notes <polynotes2021@gmail.com>
2021-10-19 17:21:57 +0530

    delete all

--- Function/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function(){
-    let nav = document.getElementById('nav');
-    nav.classList.toggle('top-scrll', window.scrollY > 0);
-})
-
-document.querySelector('#close').addEventListener('click', function(){
-    document.querySelector('.popupcontainer').style.display = "none";
-});
